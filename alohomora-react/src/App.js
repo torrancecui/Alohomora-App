5a684d2979dea2c7bb1abe0594d056d340cd6030
Torrance Cui <cuitorrance@gmail.com>
2021-06-29 15:13:19 -0700

    name is undefined bug

--- alohomora-react/src/App.js
+++ alohomora-react/src/App.js
@@ -8,45 +8,58 @@ import './App.css';
 const houses = ["hufflepuff", "gryffindor", "slytherin", "ravenclaw"];
 //random number between 0-3
 let rand_num = Math.floor( (Math.random() * 4));
-let parsed = queryString.parse(window.location.search);
-let accessToken = parsed.access_token;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p className = "App-title">
-          welcome to alohomora.
-        </p>
-        <div className = "Banner">
-          {/* <div className = "leftBanner"> 
-            <img src={hpLogo} className="App-logo" alt="hpLogo" />
-          </div> */}
-          <div className = "centerBanner"> 
-            <img src = {houses_img} className= "Houses-img" alt = "houses" />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      serverData: {},
+    }
+  }
+  //not sure if working
+  componentDidMount() {
+    let parsed = queryString.parse(window.location.search);
+    let accessToken = parsed.access_token;
+    if (!accessToken)
+      return;
+    fetch('https://api.spotify.com/v1/me', {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(response => response.json())
+    .then(data => this.setState({
+      user: {
+        name: data.display_name
+      }
+    }))
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p className = "App-title">
+            welcome to alohomora.
+          </p>
+          <div className = "Banner">
+            <div className = "centerBanner"> 
+              <img src = {houses_img} className= "Houses-img" alt = "houses" />
+            </div>
           </div>
-          {/* <div className = "rightBanner"> 
-            <img src={hpLogo} className="App-logo" alt="hpLogo" />
-          </div> */}
-        </div>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          your house is ... {houses[rand_num]}
-        </a>
-        
+          
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            your house is ... {houses[rand_num]}
+          </a>
+        </header>
         <div className = "Credits">
-          <p>developed by torrance</p>
-          <p>hp credit to ms.rowling</p>
+            <p>developed by torrance</p>
+            <p>hp credit to ms.rowling</p>
         </div>
-
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
