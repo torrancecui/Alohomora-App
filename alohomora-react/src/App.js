ccdaaae14a4967d5d6b3c803210a2781378d4efc
Torrance Cui <cuitorrance@gmail.com>
2021-10-18 16:10:19 -0700

    polished code

--- alohomora-react/src/App.js
+++ alohomora-react/src/App.js
@@ -1,62 +1,77 @@
-// import logo from './images/logo.svg';
-// import hpLogo from './images/hpLogo.svg';
-// import houses_img from "./images/houses.png";
+import React, { Component } from "react";
+import queryString from "query-string";
+import "./App.css";
+
 import loginLogo from "./images/login_logo.svg";
 import gryffindor from "./images/lion.png";
 import slytherin from "./images/snake.png";
 import hufflepuff from "./images/badger.png";
 import ravenclaw from "./images/raven.png";
-import React, { Component } from "react";
-import queryString from "query-string";
-import "./App.css";
 
-const houses = ["hufflepuff", "gryffindor", "slytherin", "ravenclaw"];
-// let rand_num = Math.floor(Math.random() * 4);
+//global for later dev
 let accessToken;
 let username = "";
 
-class Gryffindor extends Component{
-  render(){
-    return <img src = {gryffindor} className= "Houses-img" alt = "houses" />
-  }
+//return app title
+function Title(){
+  return(
+    <div class = "Title">
+        <p>welcome to alohomora.</p>
+    </div>
+  );
 }
-class Slytherin extends Component{
-  render(){
-    return <img src = {slytherin} className= "Houses-img" alt = "houses" />
-  }
+
+//return app credits
+function Credits(){
+  return(
+    <div class = "Credits">
+      <p>developed by torrance</p>
+      <p>hp credit to ms.rowling</p>
+    </div>
+  );
 }
-class Hufflepuff extends Component{
-  render(){
-    return <img src = {hufflepuff} className= "Houses-img" alt = "houses" />
-  }
+
+//constructs login page if user is not signed in
+function LoginPage(){
+  return(
+    <div class = "Login-page">
+      <div class = "Login-logo">
+        <img src = {loginLogo} class = "Login-img" alt = "login-img" />
+      </div>
+      <div class  = "Login-button">
+        <button 
+            onClick={() => {
+              window.location = window.location.href.includes("localhost")
+                ? "http://localhost:8888/login"
+                : "https://alohomora-app-backend.herokuapp.com/login";
+            }}
+          >
+            sign in with spotify
+          </button>
+      </div>
+    </div>
+  )
 }
-class Ravenclaw extends Component{
-  render(){
-    return <img src = {ravenclaw} className= "Houses-img" alt = "houses" />
-  }
-}
+
+//calculates and returns house decision and logo
 class HouseDecision extends Component{
   render(){
+    //current random algo using hash of username
+    const houses = ["hufflepuff", "gryffindor", "slytherin", "ravenclaw"];
     let hash = 0;
     for (let i = 0; i < username.length; i++) {
       hash += username.charCodeAt(i);
     }
     let hashNum = hash % 4;
-    let houseIcon;
     let house = houses[hashNum];
-    if (house === 'gryffindor'){
-      houseIcon =  <Gryffindor />;
-    }else if (house === 'slytherin'){
-      houseIcon =  <Slytherin />;
-    }else if (house === 'hufflepuff'){
-      houseIcon =  <Hufflepuff />;
-    }else if (house === 'ravenclaw'){
-      houseIcon =  <Ravenclaw />;
-    }
+
     return (
-      <div>
-        <div className = "Banner">
-          {houseIcon}
+      <div class = "Decision">
+        <div class = "Banner">
+          {house === 'gryffindor' && <img src = {gryffindor} class= "Houses-img" alt = "gryffindor" />}
+          {house === 'hufflepuff' && <img src = {hufflepuff} class= "Houses-img" alt = "hufflepuff" />}
+          {house === 'slytherin' && <img src = {slytherin} class= "Houses-img" alt = "slytherin" />}
+          {house === 'ravenclaw' && <img src = {ravenclaw} class= "Houses-img" alt = "ravenclaw" />}
         </div>
         <p>
           your house is ... {house}
@@ -66,6 +81,7 @@ class HouseDecision extends Component{
   }
 }
 
+//MAIN APP
 class App extends Component {
   constructor() {
     super();
@@ -91,41 +107,21 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <p className = "App-title">
-          welcome to alohomora.
-        </p>
+      <div class="App">
+        <Title></Title>
         {this.state.user ? (
-        //content if user is signed in already
-          <header className="App-body">
+          //content if user is signed in already
+          <div class="Content">
             <p>
               hi {username = this.state.user.name}
             </p>
             <HouseDecision />
-        </header>
-        ) : (
-        //content if user is NOT signed in
-          <div className = "Login-page">
-            <div className = "Login-logo">
-              <img src = {loginLogo} className= "Login-img" alt = "login-img" />
-            </div>
-            <div className = "Login-button">
-              <button 
-                onClick={() => {
-                  window.location = window.location.href.includes("localhost")
-                    ? "http://localhost:8888/login"
-                    : "https://alohomora-app-backend.herokuapp.com/login";
-                }}
-              >
-                sign in with spotify
-              </button>
-            </div>
           </div>
+        ) : (
+          //content if user is NOT signed in
+          <LoginPage></LoginPage>
         )}
-        <div className = "Credits">
-            <p>developed by torrance</p>
-            <p>hp credit to ms.rowling</p>
-        </div>
+        <Credits></Credits>
       </div>
     );
   }
