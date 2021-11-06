2f4f83dcfc427523816c0f70a5dd9091da28d733
Torrance Cui <cuitorrance@gmail.com>
2021-11-05 18:28:56 -0700

    working genre fetch

--- alohomora-react/src/App.js
+++ alohomora-react/src/App.js
@@ -15,7 +15,7 @@ let username = "";
 //return app title
 function Title(){
   return(
-    <div class = "Title">
+    <div className = "Title">
         <p>welcome to alohomora ϟ</p>
     </div>
   );
@@ -24,8 +24,8 @@ function Title(){
 //return app credits
 function Credits(){
   return(
-    <div class = "Credits">
-      <p>developed by torrance</p>
+    <div className = "Credits">
+      <p>developed by torrance cui & joseph nahm</p>
       <p>harry potter credits to ms.rowling</p>
     </div>
   );
@@ -34,11 +34,11 @@ function Credits(){
 //constructs login page if user is not signed in
 function LoginPage(){
   return(
-    <div class = "Login-page">
-      <div class = "Login-logo">
-        <img src = {loginLogo} class = "Login-img" alt = "login-img" />
+    <div className = "Login-page">
+      <div className = "Login-logo">
+        <img src = {loginLogo} className = "Login-img" alt = "login-img" />
       </div>
-      <div class  = "Login-button">
+      <div className  = "Login-button">
         <button 
             onClick={() => {
               window.location = window.location.href.includes("localhost")
@@ -66,9 +66,9 @@ class HouseDecision extends Component{
     let house = houses[hashNum];
 
     return (
-      <div class = "Decision">
+      <div className = "Decision">
         <div 
-          class = "Banner"
+          className = "Banner"
           style={
             { 
               background: house === 'slytherin' ? "#caffbf" : 
@@ -79,10 +79,10 @@ class HouseDecision extends Component{
             }
           }
         >
-          {house === 'gryffindor' && <img src = {gryffindor} class= "Houses-img" alt = "gryffindor" />}
-          {house === 'hufflepuff' && <img src = {hufflepuff} class= "Houses-img" alt = "hufflepuff" />}
-          {house === 'slytherin' && <img src = {slytherin} class= "Houses-img" alt = "slytherin" />}
-          {house === 'ravenclaw' && <img src = {ravenclaw} class= "Houses-img" alt = "ravenclaw" />}
+          {house === 'gryffindor' && <img src = {gryffindor} className= "Houses-img" alt = "gryffindor" />}
+          {house === 'hufflepuff' && <img src = {hufflepuff} className= "Houses-img" alt = "hufflepuff" />}
+          {house === 'slytherin' && <img src = {slytherin} className= "Houses-img" alt = "slytherin" />}
+          {house === 'ravenclaw' && <img src = {ravenclaw} className= "Houses-img" alt = "ravenclaw" />}
         </div>
         <p>
           your house is ... {house}
@@ -90,6 +90,39 @@ class HouseDecision extends Component{
       </div>
     )
   }
+}
+
+function getTopGenre(topArtists){
+  let topFiveArtists = topArtists.slice(0,5);
+
+  let genres = {
+    pop: 0,
+    randb: 0,
+    edm: 0,
+    rock: 0,
+    rap: 0,
+  }
+
+  for( const artist of topFiveArtists){
+    for (const genre of artist.genres){
+      if (genre.includes('pop')) genres.pop++;
+      if (genre.includes('r&b')) genres.randb++;
+      if (genre.includes('rock')) genres.rock++;
+      if (genre.includes('edm')) genres.edm++;
+      if (genre.includes('rap')) genres.rap++;
+    }
+  }
+  let max = 0;
+  let topGenre;
+  for (var genre in genres) {
+    if (genres[genre] > max) {
+      max = genres[genre]
+      topGenre = genre;
+    }
+  }
+  console.log(genres);
+  return topGenre;
+    
 }
 
 //MAIN APP
@@ -105,24 +138,30 @@ class App extends Component {
     accessToken = parsed.access_token;
     if (!accessToken) return;
     fetch("https://api.spotify.com/v1/me", {
-      headers: { Authorization: "Bearer " + accessToken },
-    })
-      .then((response) => response.json())
-      .then((data) =>
+      headers: { 'Authorization': "Bearer " + accessToken },
+    }).then((response) => response.json()).then((data) =>
         this.setState({
           user: {
             name: data.display_name,
           },
         })
       );
+    fetch('https://api.spotify.com/v1/me/top/artists', {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(response => response.json())
+    .then(data => {
+      let topArtists = data.items;
+      let mostListenedGenre = getTopGenre(topArtists);
+      console.log(mostListenedGenre);
+    })
   }
   render() {
     return (
-      <div class="App">
+      <div className="App">
         <Title></Title>
         {this.state.user ? (
           //content if user is signed in already
-          <div class="Content">
+          <div className="Content">
             <p>
               hi {username = this.state.user.name}
             </p>
